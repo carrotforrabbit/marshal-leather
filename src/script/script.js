90e376a318d1cb67199bbcbf513be496ab64e34a
carrotforrabbit <parasyuta.anna@gmail.com>
2022-12-13 12:36:25 +0000

    fixes bugs

--- src/script/script.js
+++ src/script/script.js
@@ -1,3 +1,5 @@
+//masonry layout
+
 window.onload = function() {
 let grid = document.querySelector('.gallery__wrapper');
 let msnry = new Masonry( grid, {
@@ -7,25 +9,26 @@ let msnry = new Masonry( grid, {
 });
 }
 
+//accordeon
 let acc = document.querySelector(".accordeon__title");
 
 acc.addEventListener("click", ({target}) => {
   target.classList.toggle('active');
 })
 
+//up button
 let upBtn = document.querySelector('.up-button');
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 1000) {
     upBtn.style.display = "block";
   } else {
     upBtn.style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
