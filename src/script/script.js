20d20a5e09f89288fb6133b002e829c8741dfc8b
carrotforrabbit <parasyuta.anna@gmail.com>
2022-12-13 12:26:12 +0000

    adds up button

--- src/script/script.js
+++ src/script/script.js
@@ -12,3 +12,21 @@ let acc = document.querySelector(".accordeon__title");
 acc.addEventListener("click", ({target}) => {
   target.classList.toggle('active');
 })
+
+let upBtn = document.querySelector('.up-button');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    upBtn.style.display = "block";
+  } else {
+    upBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
