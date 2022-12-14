bd890d706fdff440329f708d75aebb672d3e5ed9
carrotforrabbit <parasyuta.anna@gmail.com>
2022-12-14 11:11:38 +0000

    adds smooth scroll

--- src/script/script.js
+++ src/script/script.js
@@ -33,17 +33,3 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
-//smooth scroll
-const smoothLinks = document.querySelectorAll('a[href^="#"]');
-for (let smoothLink of smoothLinks) {
-    smoothLink.addEventListener('click', function (e) {
-        e.preventDefault();
-        const id = smoothLink.getAttribute('href');
-
-        document.querySelector(id).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    });
-};
